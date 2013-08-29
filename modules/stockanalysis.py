3007628db990f2bd1b4fdcdea48759af5273c395
Nick Lozon <nicklozon@gmail.com>
2013-08-28 21:45:38 -0400

    Started GoogleScreener

--- modules/stockanalysis.py
+++ modules/stockanalysis.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, \
+    Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -41,3 +42,42 @@ class StockPriceMinute(Base):
 
     def __init__(self):
         print('Stock Price Minute initialized...')
+
+
+# Description: Class that retrieves data from the Google Stock Screener.
+#              Functonality only implemented as see fit - too many fields to
+#              implement all at once.
+class GoogleScreener:
+    market_capital_min = '0'
+    market_capital_max = '1T'
+    exchange = 'TSE'
+    dividend_yield = '3'
+    dividend_quarterly_only = True
+    last_price = 0
+    restype = 'company'
+    sort_by = 'MarketCap'
+
+    def __init__(self):
+        print('Google Screener initialized...')
+
+    # Setters
+    def set_market_capital(self, min, max):
+        self.market_capital_min = min
+        self.market_capital_max = max
+        print('Market capital min/max: %s/%s' % self.market_capital_min,
+              self.market_capital_max)
+
+    def set_exchange(self, exchange):
+        self.exchange = exchange
+
+    def set_dividend_yield(self, dividend_yield):
+        self.dividend_yield = dividend_yield
+
+    def set_dividend_quarterly_only(self, flag):
+        self.dividend_quarterly_only = flag
+
+    def set_last_price(self, last_price):
+        self.last_price = last_price
+
+    def set_sort_by(self, sort_by):
+        self.sort_by = sort_by
