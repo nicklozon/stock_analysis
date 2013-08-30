761c8e6c384431989cd054b2abffbfdf1dc1288e
Nick Lozon <nicklozon@gmail.com>
2013-08-30 00:16:11 -0400

    Started building the query builder.

--- modules/stockanalysis.py
+++ modules/stockanalysis.py
@@ -54,8 +54,10 @@ class GoogleScreener:
     market_capital_min = '0'
     market_capital_max = '1T'
     exchange = 'TSE'
-    dividend_yield = '3'
-    last_price = 0
+    dividend_yield_min = 3
+    dividend_yield_max = 0
+    last_price_min = 0
+    last_price_max = 0
     result_type = 'company'
     sort_by = 'MarketCap'
     result_limit = 200
@@ -66,17 +68,19 @@ class GoogleScreener:
 
     # Setters
     def set_market_capital(self, min, max):
-        self.market_capital_min = min
-        self.market_capital_max = max
+        self.market_capital_min = str(min)
+        self.market_capital_max = str(max)
 
     def set_exchange(self, exchange):
         self.exchange = exchange
 
-    def set_dividend_yield(self, dividend_yield):
-        self.dividend_yield = dividend_yield
+    def set_dividend_yield_range(self, min, max):
+        self.dividend_yield_min = min
+        self.dividend_yield_min = max
 
-    def set_last_price(self, last_price):
-        self.last_price = last_price
+    def set_last_price_range(self, min, max):
+        self.last_price_min = min
+        self.last_price_max = max
 
     def set_sort_by(self, sort_by):
         self.sort_by = sort_by
@@ -89,7 +93,22 @@ class GoogleScreener:
 
     # Class methods
     def build_query(self):
-        return ''
+        result = '[(exchange == "TSE") & '
+        result += '(market_cap >= ' + self.market_capital_min + ') & '
+        result += '(market_cap <= ' + self.market_capital_max + ') &'\
+            if self.market_capital_max > 0 else ''
+        # This may seem redundant (last_price > 0), but it protects from bogus
+        # data. It also makes last_price appear in the returned columns.
+        result += '(last_price > ' + str(self.last_price_min) + ') & '
+        result += '(last_price <= ' + str(self.last_price_max) + ') &'\
+            if self.last_price_max > 0 else ''
+        result += '(dividend_yield >= ' + str(self.dividend_yield_min) + ') & '
+        result += '(dividend_yield <= ' + str(self.dividend_yield_max) + ') &'\
+            if self.dividend_yield_max > 0 else ''
+        # Again, may seem redundant, but this forces the recent dividend to
+        # return in the result. Should be made dynamic in future.
+        result += '(dividend_recent_quarter > 0)'
+        return result
 
     def run(self):
         # build and execute query
